c02ae233bcca79e623d0eb665b941eab6223079a
brian.musili <brian.musili@systechafrica.com>
2025-09-13 23:25:40 +0300

    Walkthrough routes
    Process Affected:

--- lib/shop_web/router.ex
+++ lib/shop_web/router.ex
@@ -17,18 +17,37 @@ defmodule ShopWeb.Router do
     plug :accepts, ["json"]
   end
 
+  pipeline :auth do
+    plug Plugs.EnsureAuthenticated
+  end
+
   scope "/", ShopWeb do
     pipe_through :browser
 
     get "/", PageController, :home
     get "/products", ProductController, :index
     get "/products/:id", ProductController, :show
+
+    # resources "/products", ProductController, except: [:delete]
+
+    # resources "/users", UserController do
+    #   get "/posts", PostController, :index
+    # end
   end
 
   # Other scopes may use custom stacks.
   # scope "/api", ShopWeb do
   #   pipe_through :api
+
+  #   get "/", PageController, :home
+  #   get "/products", ProductController, :index
+  #   get "/products/:id", ProductController, :show
   # end
+
+  scope "/dashboard", ShopWeb do
+    pipe_through [:browser, :auth]
+    get "/", DashboardController, :index
+  end
 
   # Enable LiveDashboard and Swoosh mailbox preview in development
   if Application.compile_env(:shop, :dev_routes) do
